c55888d14ef8ebe5a7ebcbcf2c68a06b7c0d7ca9
Geir <geir.drange@gmail.com>
2024-03-06 18:27:23 +0100

    Element params into dict

---
+++ tests/unit/test_element.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.append("../src")
+
+
+from sysloss.element import *
+
+
+# import sysloss
+
+
+def test1():
+    assert dymmy() == True, "Dummy test"
--- tests/unit/test_sysloss.py
+++
@@ -1,5 +0,0 @@
-from sysloss import dummy
-
-
-def test1():
-    assert dymmy() == True, "Dummy test"
